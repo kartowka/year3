b612eed1ddef00ae83dd0c939173f9c59854b666
kartowka <antonfley@gmail.com>
2020-12-07 14:00:10 +0200

    added precision to list, added function check on derivative

--- bisection_method.py
+++ bisection_method.py
@@ -29,8 +29,10 @@ def bisection(start_point, end_point, f, epsilon):
                 f"iteration number: {i+1}: {start_point} + {end_point} / 2={m}")
 
     # rounding works until 1e-4 because from there the number repr changes to "1e-x"
-    if abs(f(round(m, int(str(epsilon).split('.')[1])))) == 0 or abs(start_point - end_point) < epsilon:
-        roots.append(m)
+    if(f==f_derivative and g(m)==0):
+        roots.append((round(m,len(str(epsilon)[2:]))))
+    elif abs(f(round(m, int(str(epsilon).split('.')[1])))) == 0 or abs(start_point - end_point) < epsilon and f!=f_derivative:
+        roots.append((round(m,len(str(epsilon)[2:]))))
     else:
         print('no root for this function in given')
 
@@ -52,12 +54,13 @@ def range_bisection(min_range, max_range, section):
         if f(ranges[0]) * f(ranges[1]) < 0.:
             bisection(ranges[0], ranges[1], f, epsilon)
         if f_derivative(ranges[0]) * f_derivative(ranges[1]) < 0.:
-            bisection(ranges[0], ranges[1], f, epsilon)
+            bisection(ranges[0], ranges[1], f_derivative, epsilon)
     if (f(0)) == 0:
         roots.append(0)
 
 
 def f(x): return x**4 + x**3 - 3*x**2
+def g(x): return f(x)
 def f_derivative(x): return 4*x**3 + 3*x**2 - 6*x
 
 
